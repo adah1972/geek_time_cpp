06c1083d26859317bf400c99800f46a93d547537
Wu Yongwei <wuyongwei@gmail.com>
2020-02-16 16:54:32 +0800

    Add files for debug_new (21st column).

---
+++ 21/test_debug_new_include.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <nvwa/debug_new.h>
+
+int main()
+{
+    char* ptr = new char[20];
+}
---
+++ 21/test_debug_new_no_include.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+    char* ptr = new char[20];
+}
