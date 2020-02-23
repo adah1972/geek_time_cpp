c68141e39518b2ba3398f1c94249844ceaa63422
Wu Yongwei <wuyongwei@gmail.com>
2020-02-23 13:42:03 +0800

    Add files for the 1st column.

---
+++ 01/test01_stack_unwind.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+class Obj {
+public:
+    Obj() { puts("Obj()"); }
+    ~Obj() { puts("~Obj()"); }
+};
+
+void foo(int n)
+{
+    Obj obj;
+    if (n == 42) {
+        throw "life, the universe and everything";
+    }
+}
+
+int main()
+{
+    try {
+        foo(41);
+        foo(42);
+    }
+    catch (const char* s) {
+        puts(s);
+    }
+}
---
+++ 01/test02_shape_wrapper.cpp
@@ -0,0 +1,25 @@
+#include "shape.h"
+
+class shape_wrapper {
+public:
+    explicit shape_wrapper(shape* ptr = nullptr)
+        : ptr_(ptr) {}
+    ~shape_wrapper()
+    {
+        delete ptr_;
+    }
+    shape* get() const { return ptr_; }
+
+private:
+    shape* ptr_;
+};
+
+void foo()
+{
+    shape_wrapper ptr_wrapper(create_shape(shape_type::circle));
+}
+
+int main()
+{
+    foo();
+}
