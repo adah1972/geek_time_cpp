b290c498dc29727646ef921565a6869863ff0711
Wu Yongwei <wuyongwei@gmail.com>
2020-02-27 22:00:13 +0800

    Reformat code for consistency.

--- 30/manual_iteration/fibonacci.hpp
+++ 30/manual_iteration/fibonacci.hpp
@@ -7,87 +7,66 @@
 
 class fibonacci {
 public:
-  class sentinel;
-  class iterator;
-  iterator begin() noexcept;
-  sentinel end() noexcept;
+    class sentinel;
+    class iterator;
+    iterator begin() noexcept;
+    sentinel end() noexcept;
 };
 
 class fibonacci::sentinel {};
 
 class fibonacci::iterator {
 public:
-  // Required to satisfy iterator
-  // concept
-  typedef ptrdiff_t difference_type;
-  typedef uint64_t value_type;
-  typedef const uint64_t* pointer;
-  typedef const uint64_t& reference;
-  typedef std::input_iterator_tag
-    iterator_category;
+    // Required to satisfy iterator concept
+    typedef ptrdiff_t difference_type;
+    typedef uint64_t value_type;
+    typedef const uint64_t* pointer;
+    typedef const uint64_t& reference;
+    typedef std::input_iterator_tag iterator_category;
 
-  value_type operator*() const
-  {
-    return b_;
-  }
-  pointer operator->() const
-  {
-    return &b_;
-  }
-  iterator& operator++()
-  {
-    auto tmp = a_;
-    a_ = b_;
-    b_ += tmp;
-    return *this;
-  }
-  iterator operator++(int)
-  {
-    auto tmp = *this;
-    ++*this;
-    return tmp;
-  }
-  bool
-  operator==(const sentinel&) const
-  {
-    return false;
-  }
-  bool
-  operator!=(const sentinel&) const
-  {
-    return true;
-  }
+    value_type operator*() const { return b_; }
+    pointer operator->() const { return &b_; }
+    iterator& operator++()
+    {
+        auto tmp = a_;
+        a_ = b_;
+        b_ += tmp;
+        return *this;
+    }
+    iterator operator++(int)
+    {
+        auto tmp = *this;
+        ++*this;
+        return tmp;
+    }
+    bool operator==(const sentinel&) const { return false; }
+    bool operator!=(const sentinel&) const { return true; }
 
 private:
-  uint64_t a_{0};
-  uint64_t b_{1};
+    uint64_t a_{0};
+    uint64_t b_{1};
 };
 
-// sentinel needs to be
-// equality_comparable_with iterator
-bool operator==(
-  const fibonacci::sentinel& lhs,
-  const fibonacci::iterator& rhs)
+// sentinel needs to be equality_comparable_with iterator
+bool operator==(const fibonacci::sentinel& lhs,
+                const fibonacci::iterator& rhs)
 {
-  return rhs == lhs;
+    return rhs == lhs;
 }
-bool operator!=(
-  const fibonacci::sentinel& lhs,
-  const fibonacci::iterator& rhs)
+bool operator!=(const fibonacci::sentinel& lhs,
+                const fibonacci::iterator& rhs)
 {
-  return rhs != lhs;
+    return rhs != lhs;
 }
 
-inline fibonacci::iterator
-fibonacci::begin() noexcept
+inline fibonacci::iterator fibonacci::begin() noexcept
 {
   return iterator();
 }
 
-inline fibonacci::sentinel
-fibonacci::end() noexcept
+inline fibonacci::sentinel fibonacci::end() noexcept
 {
-  return sentinel();
+    return sentinel();
 }
 
 #endif // FIBONACCI_HPP
