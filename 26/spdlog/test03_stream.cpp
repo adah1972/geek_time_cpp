bc4c2b8ecbe61731bc07af4ca308ebb10f7de77a
Wu Yongwei <wuyongwei@gmail.com>
2023-02-26 09:02:07 +0800

    Fix build for fmt v9.0+

--- 26/spdlog/test03_stream.cpp
+++ 26/spdlog/test03_stream.cpp
@@ -1,7 +1,11 @@
 #include <vector>              // std::vector
 #include "output_container.h"  // operator<< for containers
-#include "spdlog/fmt/ostr.h"   // operator<< support in spdlog
 #include "spdlog/spdlog.h"     // spdlog
+#include "spdlog/fmt/ostr.h"   // operator<< support in spdlog
+
+#if FMT_VERSION >= 90000
+template <> struct fmt::formatter<std::vector<int>> : ostream_formatter {};
+#endif
 
 using namespace std;
 
