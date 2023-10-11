f57b9c3ad2aa3ec59f9242318008debbf2f07859
Wu Yongwei <wuyongwei@gmail.com>
2023-10-11 21:55:50 +0800

    Make REPEAT macros work on Traditional MSVC
    However, static reflection still does not fully work, as in the
    traditional MSVC preprocessor one still has problems with spaces:
    
    PAIR((int)x) => intx
    PAIR((int) x) => int x
    STRING(STRIP((int)x)) => "x"
    STRING(STRIP((int) x)) => " x"

--- 40/metamacro.h
+++ 40/metamacro.h
@@ -2,7 +2,7 @@
  * MIT License
  *
  * Copyright (c) 2021 Netcan
- * Copyright (c) 2021-2022 Wu Yongwei
+ * Copyright (c) 2021-2023 Wu Yongwei
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -39,9 +39,8 @@
     INTERNAL_EXPAND_ARGS_PRIVATE(INTERNAL_ARGS_AUGMENTER(__VA_ARGS__))
 
 #define INTERNAL_ARGS_AUGMENTER(...) unused, __VA_ARGS__
-#define INTERNAL_EXPAND(x) x
 #define INTERNAL_EXPAND_ARGS_PRIVATE(...)                                  \
-    INTERNAL_EXPAND(INTERNAL_GET_ARG_COUNT_PRIVATE(                        \
+    EXPAND(INTERNAL_GET_ARG_COUNT_PRIVATE(                                 \
         __VA_ARGS__, 100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88,  \
         87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72,    \
         71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56,    \
@@ -49,6 +48,111 @@
         39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24,    \
         23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7,   \
         6, 5, 4, 3, 2, 1, 0))
+
+#define REPEAT_0(func, i, arg)
+#define REPEAT_1(func, i, arg)        func(i, arg)
+#define REPEAT_2(func, i, arg, ...)   func(i, arg) EXPAND(REPEAT_1(func, i + 1, __VA_ARGS__))
+#define REPEAT_3(func, i, arg, ...)   func(i, arg) EXPAND(REPEAT_2(func, i + 1, __VA_ARGS__))
+#define REPEAT_4(func, i, arg, ...)   func(i, arg) EXPAND(REPEAT_3(func, i + 1, __VA_ARGS__))
+#define REPEAT_5(func, i, arg, ...)   func(i, arg) EXPAND(REPEAT_4(func, i + 1, __VA_ARGS__))
+#define REPEAT_6(func, i, arg, ...)   func(i, arg) EXPAND(REPEAT_5(func, i + 1, __VA_ARGS__))
+#define REPEAT_7(func, i, arg, ...)   func(i, arg) EXPAND(REPEAT_6(func, i + 1, __VA_ARGS__))
+#define REPEAT_8(func, i, arg, ...)   func(i, arg) EXPAND(REPEAT_7(func, i + 1, __VA_ARGS__))
+#define REPEAT_9(func, i, arg, ...)   func(i, arg) EXPAND(REPEAT_8(func, i + 1, __VA_ARGS__))
+#define REPEAT_10(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_9(func, i + 1, __VA_ARGS__))
+#define REPEAT_11(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_10(func, i + 1, __VA_ARGS__))
+#define REPEAT_12(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_11(func, i + 1, __VA_ARGS__))
+#define REPEAT_13(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_12(func, i + 1, __VA_ARGS__))
+#define REPEAT_14(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_13(func, i + 1, __VA_ARGS__))
+#define REPEAT_15(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_14(func, i + 1, __VA_ARGS__))
+#define REPEAT_16(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_15(func, i + 1, __VA_ARGS__))
+#define REPEAT_17(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_16(func, i + 1, __VA_ARGS__))
+#define REPEAT_18(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_17(func, i + 1, __VA_ARGS__))
+#define REPEAT_19(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_18(func, i + 1, __VA_ARGS__))
+#define REPEAT_20(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_19(func, i + 1, __VA_ARGS__))
+#define REPEAT_21(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_20(func, i + 1, __VA_ARGS__))
+#define REPEAT_22(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_21(func, i + 1, __VA_ARGS__))
+#define REPEAT_23(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_22(func, i + 1, __VA_ARGS__))
+#define REPEAT_24(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_23(func, i + 1, __VA_ARGS__))
+#define REPEAT_25(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_24(func, i + 1, __VA_ARGS__))
+#define REPEAT_26(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_25(func, i + 1, __VA_ARGS__))
+#define REPEAT_27(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_26(func, i + 1, __VA_ARGS__))
+#define REPEAT_28(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_27(func, i + 1, __VA_ARGS__))
+#define REPEAT_29(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_28(func, i + 1, __VA_ARGS__))
+#define REPEAT_30(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_29(func, i + 1, __VA_ARGS__))
+#define REPEAT_31(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_30(func, i + 1, __VA_ARGS__))
+#define REPEAT_32(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_31(func, i + 1, __VA_ARGS__))
+#define REPEAT_33(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_32(func, i + 1, __VA_ARGS__))
+#define REPEAT_34(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_33(func, i + 1, __VA_ARGS__))
+#define REPEAT_35(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_34(func, i + 1, __VA_ARGS__))
+#define REPEAT_36(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_35(func, i + 1, __VA_ARGS__))
+#define REPEAT_37(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_36(func, i + 1, __VA_ARGS__))
+#define REPEAT_38(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_37(func, i + 1, __VA_ARGS__))
+#define REPEAT_39(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_38(func, i + 1, __VA_ARGS__))
+#define REPEAT_40(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_39(func, i + 1, __VA_ARGS__))
+#define REPEAT_41(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_40(func, i + 1, __VA_ARGS__))
+#define REPEAT_42(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_41(func, i + 1, __VA_ARGS__))
+#define REPEAT_43(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_42(func, i + 1, __VA_ARGS__))
+#define REPEAT_44(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_43(func, i + 1, __VA_ARGS__))
+#define REPEAT_45(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_44(func, i + 1, __VA_ARGS__))
+#define REPEAT_46(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_45(func, i + 1, __VA_ARGS__))
+#define REPEAT_47(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_46(func, i + 1, __VA_ARGS__))
+#define REPEAT_48(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_47(func, i + 1, __VA_ARGS__))
+#define REPEAT_49(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_48(func, i + 1, __VA_ARGS__))
+#define REPEAT_50(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_49(func, i + 1, __VA_ARGS__))
+#define REPEAT_51(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_50(func, i + 1, __VA_ARGS__))
+#define REPEAT_52(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_51(func, i + 1, __VA_ARGS__))
+#define REPEAT_53(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_52(func, i + 1, __VA_ARGS__))
+#define REPEAT_54(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_53(func, i + 1, __VA_ARGS__))
+#define REPEAT_55(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_54(func, i + 1, __VA_ARGS__))
+#define REPEAT_56(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_55(func, i + 1, __VA_ARGS__))
+#define REPEAT_57(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_56(func, i + 1, __VA_ARGS__))
+#define REPEAT_58(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_57(func, i + 1, __VA_ARGS__))
+#define REPEAT_59(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_58(func, i + 1, __VA_ARGS__))
+#define REPEAT_60(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_59(func, i + 1, __VA_ARGS__))
+#define REPEAT_61(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_60(func, i + 1, __VA_ARGS__))
+#define REPEAT_62(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_61(func, i + 1, __VA_ARGS__))
+#define REPEAT_63(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_62(func, i + 1, __VA_ARGS__))
+#define REPEAT_64(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_63(func, i + 1, __VA_ARGS__))
+#define REPEAT_65(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_64(func, i + 1, __VA_ARGS__))
+#define REPEAT_66(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_65(func, i + 1, __VA_ARGS__))
+#define REPEAT_67(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_66(func, i + 1, __VA_ARGS__))
+#define REPEAT_68(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_67(func, i + 1, __VA_ARGS__))
+#define REPEAT_69(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_68(func, i + 1, __VA_ARGS__))
+#define REPEAT_70(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_69(func, i + 1, __VA_ARGS__))
+#define REPEAT_71(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_70(func, i + 1, __VA_ARGS__))
+#define REPEAT_72(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_71(func, i + 1, __VA_ARGS__))
+#define REPEAT_73(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_72(func, i + 1, __VA_ARGS__))
+#define REPEAT_74(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_73(func, i + 1, __VA_ARGS__))
+#define REPEAT_75(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_74(func, i + 1, __VA_ARGS__))
+#define REPEAT_76(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_75(func, i + 1, __VA_ARGS__))
+#define REPEAT_77(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_76(func, i + 1, __VA_ARGS__))
+#define REPEAT_78(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_77(func, i + 1, __VA_ARGS__))
+#define REPEAT_79(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_78(func, i + 1, __VA_ARGS__))
+#define REPEAT_80(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_79(func, i + 1, __VA_ARGS__))
+#define REPEAT_81(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_80(func, i + 1, __VA_ARGS__))
+#define REPEAT_82(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_81(func, i + 1, __VA_ARGS__))
+#define REPEAT_83(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_82(func, i + 1, __VA_ARGS__))
+#define REPEAT_84(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_83(func, i + 1, __VA_ARGS__))
+#define REPEAT_85(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_84(func, i + 1, __VA_ARGS__))
+#define REPEAT_86(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_85(func, i + 1, __VA_ARGS__))
+#define REPEAT_87(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_86(func, i + 1, __VA_ARGS__))
+#define REPEAT_88(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_87(func, i + 1, __VA_ARGS__))
+#define REPEAT_89(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_88(func, i + 1, __VA_ARGS__))
+#define REPEAT_90(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_89(func, i + 1, __VA_ARGS__))
+#define REPEAT_91(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_90(func, i + 1, __VA_ARGS__))
+#define REPEAT_92(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_91(func, i + 1, __VA_ARGS__))
+#define REPEAT_93(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_92(func, i + 1, __VA_ARGS__))
+#define REPEAT_94(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_93(func, i + 1, __VA_ARGS__))
+#define REPEAT_95(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_94(func, i + 1, __VA_ARGS__))
+#define REPEAT_96(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_95(func, i + 1, __VA_ARGS__))
+#define REPEAT_97(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_96(func, i + 1, __VA_ARGS__))
+#define REPEAT_98(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_97(func, i + 1, __VA_ARGS__))
+#define REPEAT_99(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_98(func, i + 1, __VA_ARGS__))
+#define REPEAT_100(func, i, arg, ...)  func(i, arg) EXPAND(REPEAT_99(func, i + 1, __VA_ARGS__))
+
+#define REPEAT_ON(func, ...) \
+    EXPAND(PASTE(REPEAT_, GET_ARG_COUNT(__VA_ARGS__))(func, 0, __VA_ARGS__))
 
 #else  // Other compilers
 
@@ -83,19 +187,6 @@
         25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, \
         8, 7, 6, 5, 4, 3, 2, 1, 0)
 #endif
-
-#endif
-
-#define INTERNAL_GET_ARG_COUNT_PRIVATE(                                    \
-    e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15,  \
-    e16, e17, e18, e19, e20, e21, e22, e23, e24, e25, e26, e27, e28, e29,  \
-    e30, e31, e32, e33, e34, e35, e36, e37, e38, e39, e40, e41, e42, e43,  \
-    e44, e45, e46, e47, e48, e49, e50, e51, e52, e53, e54, e55, e56, e57,  \
-    e58, e59, e60, e61, e62, e63, e64, e65, e66, e67, e68, e69, e70, e71,  \
-    e72, e73, e74, e75, e76, e77, e78, e79, e80, e81, e82, e83, e84, e85,  \
-    e86, e87, e88, e89, e90, e91, e92, e93, e94, e95, e96, e97, e98, e99,  \
-    e100, count, ...)                                                      \
-    count
 
 #define REPEAT_0(func, i, arg)
 #define REPEAT_1(func, i, arg)        func(i, arg)
@@ -202,15 +293,28 @@
 #define REPEAT_ON(func, ...) \
     PASTE(REPEAT_, GET_ARG_COUNT(__VA_ARGS__))(func, 0, __VA_ARGS__)
 
+#endif
+
+#define INTERNAL_GET_ARG_COUNT_PRIVATE(                                    \
+    e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15,  \
+    e16, e17, e18, e19, e20, e21, e22, e23, e24, e25, e26, e27, e28, e29,  \
+    e30, e31, e32, e33, e34, e35, e36, e37, e38, e39, e40, e41, e42, e43,  \
+    e44, e45, e46, e47, e48, e49, e50, e51, e52, e53, e54, e55, e56, e57,  \
+    e58, e59, e60, e61, e62, e63, e64, e65, e66, e67, e68, e69, e70, e71,  \
+    e72, e73, e74, e75, e76, e77, e78, e79, e80, e81, e82, e83, e84, e85,  \
+    e86, e87, e88, e89, e90, e91, e92, e93, e94, e95, e96, e97, e98, e99,  \
+    e100, count, ...)                                                      \
+    count
+
 #define CONCAT(x, y) x##y
 #define PASTE(x, y) CONCAT(x, y)
 
 #define STR(x) #x
 #define STRING(x) STR(x)
 
-#define PARE(...) __VA_ARGS__
+#define EXPAND(...) __VA_ARGS__
 #define EAT(...)
-#define PAIR(x) PARE x  // PAIR((int) x) => PARE(int) x => int x
-#define STRIP(x) EAT x  // STRIP((int) x) => EAT(int) x => x
+#define PAIR(x) EXPAND x  // PAIR((int) x) => EXPAND(int) x => int x
+#define STRIP(x) EAT x    // STRIP((int) x) => EAT(int) x => x
 
 #endif // METAMACRO_H
