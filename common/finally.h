24b913e02e94dc4d4af337b57945d0cd21a4ce4c
Wu Yongwei <wuyongwei@gmail.com>
2025-02-26 10:51:06 +0800

    Add on_return for postcondition check

--- common/finally.h
+++ common/finally.h
@@ -1,6 +1,7 @@
 #ifndef FINALLY_H
 #define FINALLY_H
 
+#include <exception>    // std::uncaught_exception(s)
 #include <type_traits>  // std::decay_t
 #include <utility>      // std::exchange/forward/move
 
@@ -40,6 +41,52 @@ template <typename T>
 auto finally(T&& action)
 {
     return final_action<std::decay_t<T>>(std::forward<T>(action));
+}
+
+// Similar to final_action, with the differences that the contained
+// action will not be invoked when there are uncaught exceptions.
+template <typename T>
+class on_return_action {
+public:
+    explicit on_return_action(T action) : action_(std::move(action)) {}
+
+    on_return_action(on_return_action&& other) noexcept
+        : action_(std::move(other.action_)),
+          is_active_(std::exchange(other.is_active_, false))
+    {
+    }
+
+    on_return_action(const on_return_action&) = delete;
+    on_return_action& operator=(const on_return_action&) = delete;
+    on_return_action& operator=(on_return_action&&) = delete;
+
+    ~on_return_action()
+    {
+        if (is_active_ &&
+#if __cplusplus >= 201703L
+            std::uncaught_exceptions() == 0
+#else
+            !std::uncaught_exception()
+#endif
+        ) {
+            action_();
+        }
+    }
+
+    void dismiss() noexcept
+    {
+        is_active_ = false;
+    }
+
+private:
+    T action_;
+    bool is_active_{true};
+};
+
+template <typename T>
+auto on_return(T&& action)
+{
+    return on_return_action<std::decay_t<T>>(std::forward<T>(action));
 }
 
 #endif // FINALLY_H
