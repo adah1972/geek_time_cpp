28ce8307551ce49d1da5626b838c6a9657e8cbc0
Wu Yongwei <wuyongwei@gmail.com>
2025-02-27 10:38:04 +0800

    Fix on_return behaviour
    when the containing function is invoked during exception propagation.
    The use of C++17 or later is now required.

--- common/finally.h
+++ common/finally.h
@@ -1,7 +1,7 @@
 #ifndef FINALLY_H
 #define FINALLY_H
 
-#include <exception>    // std::uncaught_exception(s)
+#include <exception>    // std::uncaught_exceptions
 #include <type_traits>  // std::decay_t
 #include <utility>      // std::exchange/forward/move
 
@@ -43,44 +43,35 @@ auto finally(T&& action)
     return final_action<std::decay_t<T>>(std::forward<T>(action));
 }
 
-// Similar to final_action, with the differences that the contained
-// action will not be invoked when there are uncaught exceptions.
+#if __cpp_lib_uncaught_exceptions >= 201411L
+
+// Similar to final_action but with the key difference that the
+// contained action will not be invoked during exception propagation.
+// Requires C++17 or later.
 template <typename T>
 class on_return_action {
 public:
-    explicit on_return_action(T action) : action_(std::move(action)) {}
-
-    on_return_action(on_return_action&& other) noexcept
-        : action_(std::move(other.action_)),
-          is_active_(std::exchange(other.is_active_, false))
+    explicit on_return_action(T action)
+        : action_(std::move(action)),
+          uncaught_exceptions_on_entry_(std::uncaught_exceptions())
     {
     }
 
     on_return_action(const on_return_action&) = delete;
+    on_return_action(on_return_action&&) = delete;
     on_return_action& operator=(const on_return_action&) = delete;
     on_return_action& operator=(on_return_action&&) = delete;
 
     ~on_return_action()
     {
-        if (is_active_ &&
-#if __cpp_lib_uncaught_exceptions >= 201411L
-            std::uncaught_exceptions() == 0
-#else
-            !std::uncaught_exception()
-#endif
-        ) {
+        if (std::uncaught_exceptions() == uncaught_exceptions_on_entry_) {
             action_();
         }
     }
 
-    void dismiss() noexcept
-    {
-        is_active_ = false;
-    }
-
 private:
     T action_;
-    bool is_active_{true};
+    int uncaught_exceptions_on_entry_;
 };
 
 template <typename T>
@@ -88,5 +79,7 @@ auto on_return(T&& action)
 {
     return on_return_action<std::decay_t<T>>(std::forward<T>(action));
 }
+
+#endif
 
 #endif // FINALLY_H
